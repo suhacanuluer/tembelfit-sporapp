bd61010946da104d167f30fc64e0f714e18503c6
suhacanuluer <sculuer@gmail.com>
2020-09-05 22:52:47 +0300

    app updated

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/sport", /*checkAuth,*/ sportRouter);
+app.use("/sport", checkAuth, sportRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
