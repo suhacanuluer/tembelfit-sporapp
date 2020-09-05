c84a7ee579c91dec94c83232e4a95b78b338f43b
suhacanuluer <sculuer@gmail.com>
2020-09-05 22:50:12 +0300

    route updated

--- routes/sports.js
+++ routes/sports.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { SportCategories, SportSubCategories } = require("../Database/Database");
+const { SportCategories, SportSubCategories, SportDetailsCategories, SportDetails } = require("../Database/Database");
 
 router.get("/categories", (req, res) => {
     SportCategories.findAll().then(categories => {
@@ -32,17 +32,10 @@ router.get("/subcategories/:cat_id", (req, res) => {
             cat_id: req.params.cat_id
         }
     }).then(subCategories => {
-        if (subCategories) {
-            res.json({
-                status: "success",
-                data: subCategories
-            });
-        } else {
-            res.status(404).json({
-                status: "error",
-                message: "category not found"
-            });
-        }
+        res.json({
+            status: "success",
+            data: subCategories
+        });
     });
 });
 
@@ -53,6 +46,62 @@ router.post("/subcategories", (req, res) => {
         res.json({
             status: "success",
             data: subCategories
+        });
+    }, (e) => {
+        res.status(500).json({
+            status: "error"
+        });
+    });
+});
+
+router.get("/detailcategories/:subcat_id", (req, res)=> {
+    SportDetailsCategories.findAll({
+        where: {
+            subcat_id: req.params.subcat_id
+        }
+    }).then(subcatDetails => {
+        res.json({
+            status: "success",
+            data: subcatDetails
+        });
+    });
+});
+
+router.post("/detailcategories", (req, res) => {
+    const { subcat_id, title } = req.body;
+
+    SportDetailsCategories.create(req.body).then(detailCategories => {
+        res.json({
+            status: "success",
+            data: detailCategories
+        });
+    }, (e) => {
+        res.status(500).json({
+            status: "error"
+        });
+    });
+});
+
+router.get("/details/:detailscat_id", (req, res) => {
+    SportDetails.findAll({
+        where: {
+            detailscat_id: req.params.detailscat_id
+        }
+    }).then(details => {
+        res.json({
+            status: "success",
+            data: details
+        });
+    })
+})
+
+router.post("/details", (req,res) => {
+    const { detailscat_id, url_link, image } = req.body;
+
+    SportDetails.create(req.body).then(details => {
+        res.json({
+            status: "success",
+            date: details
         });
     }, (e) => {
         res.status(500).json({
